992728dd10de4644bff26661a6a0aa2bfdc3b827
Anupama Dhir <sabhssabhs@yahoo.com>
2016-07-22 13:31:35 +0800

    changed layout file

--- app.rb
+++ app.rb
@@ -12,9 +12,24 @@ class RedMartSinatraApp < Sinatra::Base
     erb:'users/index'
   end
 
+  get '/users/new' do
+    
+    erb:'users/new'
+  end
+
   get '/users/:id' do
     @user_id = User.find(params[:id])
     erb:'users/show'
   end
 
+  post '/users' do
+      puts params
+      puts params[:user]
+      #create user name, do it this way so you can check if user can be saved
+      @new_user = User.new(params[:user])
+      if @new_user.save
+        redirect('/users')
+      else
+      end
+    end
 end
