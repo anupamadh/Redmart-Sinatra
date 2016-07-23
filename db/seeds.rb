1f07aed9b31ae9e91aeed98f6c33da7cd2c5a115
Anupama Dhir <sabhssabhs@yahoo.com>
2016-07-23 14:19:09 +0800

    changed seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,5 @@ require 'faker'
 
 # seeds.db is a way of automating the population of the database with testing data
 (0..10).each do |i|
-  Product.create(name: Faker::Commerce.product_name)
+  Brand.create(name: Faker::Commerce.product_name)
 end
