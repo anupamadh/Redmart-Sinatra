b90e4da5f8609e75c7697a74975645a2904d37d1
Anupama Dhir <sabhssabhs@yahoo.com>
2016-07-23 14:16:44 +0800

    changed seeds, new.erb for for products and users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-require './models/product'
+require './models/brand'
 require 'faker'
 
 # seeds.db is a way of automating the population of the database with testing data
 (0..10).each do |i|
-  Product.create(name: Faker::Commerce.product_name, price:Faker::Commerce.price )
+  Product.create(name: Faker::Commerce.product_name)
 end
